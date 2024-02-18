e58591689f88d2fbd8c922180bee01fa498da90d
easonl0528 <160371246+easonl0528@users.noreply.github.com>
2024-02-19 02:55:49 +0800

    更改layout和sidebar

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          成耀科技
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          成耀专属GPT
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "成耀科技",
+  description: "成耀专属GPT",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "成耀科技",
     statusBarStyle: "default",
   },
 };
